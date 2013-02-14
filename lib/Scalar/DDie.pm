9005e5cfd7a2b3d8431c5d974bd073d9fc7f0ca1
tokuhirom <tokuhirom@gmail.com>
2013-02-14 17:18:39 +0900

    initial import

---
+++ lib/Scalar/DDie.pm
@@ -0,0 +1,41 @@
+package Scalar::DDie;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Scalar::DDie - ...
+
+=head1 SYNOPSIS
+
+  use Scalar::DDie;
+
+=head1 DESCRIPTION
+
+Scalar::DDie is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
